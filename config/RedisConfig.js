fcc659bfeed55af584032891eca5fde907199cbf
Nixeld <ethanongchuanming@gmail.com>
2024-01-22 22:58:56 +0800

    dsgsdg

--- config/RedisConfig.js
+++ config/RedisConfig.js
@@ -1,4 +1,22 @@
-const redis = require('redis');
+// const redis = require('redis');
+
+// // Retrieve Redis connection details from environment variables
+// const redisHost = process.env.REDIS_HOST;
+// const redisPort = process.env.REDIS_PORT;
+// const redisPassword = process.env.REDIS_PASSWORD;
+
+// // Create Redis client
+// const client = redis.createClient({ redisHost, redisPort });
+
+// // Handle Redis connection errors
+// client.on('error', (error) => {
+//     console.error('Error connecting to Redis:', error);
+// });
+
+// // Export the Redis client
+// module.exports = client;
+
+const Redis = require('ioredis');
 
 // Retrieve Redis connection details from environment variables
 const redisHost = process.env.REDIS_HOST;
@@ -6,7 +24,11 @@ const redisPort = process.env.REDIS_PORT;
 const redisPassword = process.env.REDIS_PASSWORD;
 
 // Create Redis client
-const client = redis.createClient({ redisHost, redisPort });
+const client = new Redis({
+  host: redisHost,
+  port: redisPort,
+  tls: {}, // Add this empty tls field.
+});
 
 // Handle Redis connection errors
 client.on('error', (error) => {
